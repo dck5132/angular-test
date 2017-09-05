647eb942da0b8d83ddae7c25edd7186c2073143f
Cyril Kalikin <dck5132@gmail.com>
2017-09-05 14:13:35 +0000

    Added Several Components, Services and modified configuration files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+import { AuthguardService } from './auth-guard.service';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  constructor( 
+    private authentication: AuthenticationService,
+    private authguard: AuthguardService
+    ) {}
+  
+  title = `IONX`;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { AssetsComponent } from './assets/assets.component';
+
+import { AuthenticationService } from './authentication.service';
+import { AuthguardService } from './auth-guard.service';
+import { AssetService } from './asset.service';
+import { AssetComponent } from './asset/asset.component';
+
+const appRoutes: Routes = [
+   { path: '', component: WelcomeComponent},
+   { path: 'login', component: LoginComponent},
+   { path: 'assets', component: AssetsComponent, canActivate: [AuthguardService] },
+   { path: 'assets/:name', component: AssetComponent, canActivate: [AuthguardService] }
+ ];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    AssetsComponent,
+    AssetComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true })
   ],
-  providers: [],
+  providers: [AuthenticationService, AuthguardService, AssetService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
